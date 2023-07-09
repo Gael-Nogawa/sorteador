239d5a1fc86d5b257f803663e905159fe508f6d4
Gael Eiji Nogawa <gael.nogawa@gmail.com>
2023-07-09 20:47:30 -0300

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('form-sorteador').addEventListener('submit', function(e){
+        e.preventDefault();
+        let numeroMax = document.getElementById('numero-maximo').value;
+        numeroMax = parseInt(numeroMax);
+
+        let numeroAleatório = Math.random()* numeroMax;
+        numeroAleatório = Math.floor(numeroAleatório + 1);
+        // Arrendondou pra baixo porque foi acrescentado + 1.
+        //Math.floor -> arrendonda pra baixo
+        // Math.ceil -> arrendonda pra cima
+        // Math.round -> .5+ arrendonda pra cima, e .4- arrendonda pra baixo
+      
+        console.log("O valor é: " + numeroMax)
+
+
+        document.getElementById('resultado-valor').innerText = `O valor é ${numeroAleatório}`;
+        document.querySelector('.resultado').style.display = 'block';
+    })
+})
